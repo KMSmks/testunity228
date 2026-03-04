82c6cfd5bcc8ae08223f0a17bf7cdee66a295284
KMSmks <aconand4me@gmail.com>
2026-03-04 08:58:30 +0000

    5 лаба

---
+++ hello-docker/main.go
@@ -0,0 +1,15 @@
+package main
+import (
+  "fmt"
+  "net/http"
+  "github.com/sirupsen/logrus"
+)
+func handle(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "Hello docker!")
+}
+func main() {
+  logrus.Info("App run")
+  if err := http.ListenAndServe(":8080", http.HandlerFunc(handle)); err != nil {
+    logrus.Error(err)
+  }
+}
